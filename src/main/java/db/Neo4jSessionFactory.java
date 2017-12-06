f4ad6b8a7a306da27d98301437d2a480a77b3777
Alexandre Zua Caldeira <zuacaldeira@gmail.com>
2017-12-06 08:42:06 +0100

    Use graphdb configuration for production db.

--- src/main/java/db/Neo4jSessionFactory.java
+++ src/main/java/db/Neo4jSessionFactory.java
@@ -5,8 +5,6 @@
  */
 package db;
 
-import org.neo4j.ogm.authentication.UsernamePasswordCredentials;
-import org.neo4j.ogm.config.Configuration;
 import org.neo4j.ogm.session.Session;
 import org.neo4j.ogm.session.SessionFactory;
 
@@ -23,12 +21,7 @@ public class Neo4jSessionFactory {
     private final SessionFactory sessionFactory;
     
     private Neo4jSessionFactory() {
-        Configuration configuration = new Configuration("ogm.properties");
-        UsernamePasswordCredentials userCredentials = (UsernamePasswordCredentials)configuration.driverConfiguration().getCredentials();
-        System.out.println(configuration.driverConfiguration().getDriverClassName());
-        System.out.println(userCredentials.getUsername());
-        System.out.println(userCredentials.getPassword());
-        sessionFactory = new SessionFactory(configuration, "db");
+        sessionFactory = new SessionFactory("db");
     }
     
     public static Neo4jSessionFactory getInstance() {
