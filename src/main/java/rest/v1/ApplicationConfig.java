4b004215270f2dd6a2b88589016d26991bbae158
Alexandre Zua Caldeira <zuacaldeira@gmail.com>
2018-06-10 17:33:18 +0200

    Moved rest layer to separated project.

--- src/main/java/rest/v1/ApplicationConfig.java
+++
@@ -1,6 +0,0 @@
-package rest.v1;
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("rest/v1") // set the path to REST web services
-public class ApplicationConfig extends Application {}
